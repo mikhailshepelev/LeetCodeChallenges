59aa19fa030acb4b927b7d934afa021f68ec1351
mikhailshepelev <romanist-mike@yandex.ru>
2023-04-28 13:31:35 +0300

    Add problem #19

---
+++ src/medium/_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,24 @@
+package medium;
+
+public class _19_Remove_Nth_Node_From_End_of_List {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 1; i <= n+1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
